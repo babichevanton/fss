c53c4323d0d4a015506e18e2184787fbb75bc88a
Anton Babichev <bav.dandellion@gmail.com>
2014-09-08 02:25:59 +0400

    Added POJO-class for Administration entity, added Hibernate configuretion for it

---
+++ System/src/springapp/fss/pojo/Administrator.java
@@ -0,0 +1,30 @@
+package springapp.fss.pojo;
+
+public class Administrator {
+/*---------------------------------Columns---------------------------------*/
+	private Integer id;
+	private String login;
+	private String password;
+/*---------------------------------Actions---------------------------------*/
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
